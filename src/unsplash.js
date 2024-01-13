c7f93e867ab19ede54f16c3c611f0bfda925bafa
Maurits <mcb.westbroek@gmail.com>
2024-01-13 12:24:54 +0000

    Add num_results parameter

--- src/unsplash.js
+++ src/unsplash.js
@@ -1,13 +1,14 @@
 import axios from 'axios'; 
 import { auth } from './secrets';
 
-const searchImages = async (term) => {
+const searchImages = async (term, num_results) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
         headers: {
             Authorization: `Client-ID ${auth}`
         },
         params: {
-            query: term
+            query: term,
+            per_page: num_results
         }
     });
 
