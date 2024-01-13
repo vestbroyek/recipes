3f1ac2ddbdb62963771b0c7ffcdb144cb2ba2774
Maurits <mcb.westbroek@gmail.com>
2024-01-13 11:30:34 +0000

    Configure Redux store

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { recipesApi } from "./apis/recipesApi";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from '@reduxjs/toolkit/query';
+
+export const store = configureStore({
+    reducer: {
+        [recipesApi.reducerPath]: recipesApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(recipesApi.middleware);
+    },
+});
+
+setupListeners(store.dispatch);
+
+export { useFetchRecipesQuery } from './apis/recipesApi';
